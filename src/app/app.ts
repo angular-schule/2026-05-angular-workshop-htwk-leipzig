beef74834d46c718fc6619aa2ccc9e86e505210e
Ferdinand Malcher <ferdinand@malcher.media>
2026-05-12 16:26:39 +0200

    Dashboard und Signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardPage } from "./dashboard-page/dashboard-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardPage],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  protected headline = signal('Book Rating');
+
+  setHeadline(headline: string) {
+    this.headline.set(headline);
+  }
+}
